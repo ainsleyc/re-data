127995bee4b52665e88dd7539b7eb3c6be91a23f
Ainsley Chong <ainsley.chong@gmail.com>
2016-01-23 19:15:55 -0800

    Results Annotations

--- results.go
+++ results.go
@@ -7,5 +7,30 @@ import (
 func ParseResults (data []byte) ([]byte, error) {
   respJson, _ := simplejson.NewJson(data)
   results, err := respJson.Get("map").Get("properties").EncodePretty() 
+
+  // data structure
+  // [
+  //   ?
+  //   ?
+  //   ?
+  //   Price ($ K/?)
+  //   ?
+  //   ?
+  //   ?
+  //   [
+  //     Price ($ K/?)
+  //     Beds
+  //     Baths
+  //     ?
+  //     ?
+  //     Image URL
+  //     Size string
+  //     [
+  //       ?
+  //       Open House
+  //     ]
+  //   ]
+  // ]
+
   return results, err 
 }
